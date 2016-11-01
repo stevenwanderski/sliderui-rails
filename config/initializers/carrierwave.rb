5eae48c44c334e32521b19c869c9cd996c76db91
Steven Wanderski <steven.wanderski@gmail.com>
2016-11-01 08:04:38 -0500

    Adds correct AWS setup

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,9 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = ENV['S3_BUCKET']
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
-    }
-  end
-else
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.asset_host = ActionController::Base.asset_host
-  end
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['S3_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET'],
+  }
 end
