49c7894920f8ef6b62147e56ce66ad10bfe3c3df
Steven Wanderski <steven.wanderski@gmail.com>
2016-12-28 11:25:52 -0600

    Adds unauthenticated new slider feature specs

--- spec/features/unauthenticated_flow/new_slider_page_spec.rb
+++ spec/features/unauthenticated_flow/new_slider_page_spec.rb
@@ -1,15 +1,39 @@
 require 'spec_helper'
 include UnauthenticatedSliderHelper
+include AuthHelper
 
 describe 'Unauthenticated Slider Flow', js: true do
-  before :each do
-    create_temp_slider
-  end
-
   describe 'New slider page' do
     it 'creates a new slider' do
+      create_temp_slider
       expect(Slider.count).to eq(1)
       expect(Slider.first.title).to eq('Puff Tentacle')
+    end
+
+    context 'user is authenticated' do
+      context 'user is confirmed' do
+        it 'redirects to authenticated sliders page' do
+          user = create(:user, password: 'test', confirmed: true)
+          login(user.email, 'test')
+          visit '/temp/slider/new'
+          expect(current_path).to eq('/app/sliders')
+        end
+      end
+
+      context 'user is not confirmed' do
+        it 'renders page' do
+          create_temp_slider
+          visit '/temp/slider/new'
+          expect(current_path).to eq('/temp/slider/new')
+        end
+      end
+    end
+
+    context 'user is not authenticated' do
+      it 'renders page' do
+        visit '/temp/slider/new'
+        expect(current_path).to eq('/temp/slider/new')
+      end
     end
   end
 end
