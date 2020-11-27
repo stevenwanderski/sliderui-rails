dbe9abf22ca1acf7604ac2708a0de6fc1f893cf8
Steven <steven.wanderski@gmail.com>
2020-11-27 09:45:41 -0600

    Removes unused JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,2 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-// Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
