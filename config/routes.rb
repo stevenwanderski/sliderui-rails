3997a1504fbfdb70fea2d9b816688fcd962b8f53
Steven <steven.wanderski@gmail.com>
2020-12-08 14:11:59 -0600

    Adds subscription page

--- config/routes.rb
+++ config/routes.rb
@@ -50,5 +50,6 @@ Rails.application.routes.draw do
     end
 
     resource :account, only: [:edit, :update], controller: 'account'
+    resource :subscription, only: [:edit, :update], controller: 'subscription'
   end
 end
