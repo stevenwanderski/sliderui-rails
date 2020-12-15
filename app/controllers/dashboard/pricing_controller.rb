71814fff23aca636318f1f5d33d89c4a40689260
Steven <steven.wanderski@gmail.com>
2020-12-15 17:59:29 -0600

    Adds check existing subscription to pricing controller

--- app/controllers/dashboard/pricing_controller.rb
+++ app/controllers/dashboard/pricing_controller.rb
@@ -1,6 +1,7 @@
 class Dashboard::PricingController < DashboardController
   layout 'application'
   skip_before_action :ensure_subscription
+  before_action :check_existing_subscription
 
   def index
   end
@@ -10,7 +11,15 @@ class Dashboard::PricingController < DashboardController
       subscription_type: 'free',
       subscription_status: 'active'
     )
-    
+
     redirect_to dashboard_sliders_path
+  end
+
+  private
+
+  def check_existing_subscription
+    if current_user.subscription_type.present?
+      redirect_to dashboard_sliders_path
+    end
   end
 end
