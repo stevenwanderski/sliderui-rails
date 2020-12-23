98df4aef34e201164d827ae66c4cb71aa579744a
Steven <steven.wanderski@gmail.com>
2020-12-23 06:49:27 -0600

    Adds free and premium feature specs

--- spec/features/sliders_spec.rb
+++ spec/features/sliders_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Sliders', js: true do
+describe 'Sliders Dashboard', js: true do
   let(:user) { create(:user) }
 
   before do
@@ -37,11 +37,28 @@ describe 'Sliders', js: true do
     expect(page).to have_content("Slider \"Hot Rats\" was deleted.")
   end
 
+  it 'shows the embed instructions' do
+    slider = create(:slider, user: user)
+    visit dashboard_slider_embed_path(slider)
+    expect(page).to have_content('Click here to copy code')
+  end
+
+  context 'no sliders exist' do
+    it 'shows the empty message' do
+      visit dashboard_sliders_path
+      expect(page).to have_content('Create your first slider!')
+    end
+  end
+
   describe 'free plan' do
     context 'one or more sliders exist' do
       let!(:slider1) { create(:slider, user: user, title: 'Hot Rats') }
-      let!(:slider2) { create(:slider, user: user, title: 'Apostrophe', restricted: true) }
-      let!(:slider3) { create(:slider, user: user, title: 'Overnight Sensation', restricted: true) }
+      let!(:slider2) { create(:slider, user: user, title: 'Apostrophe') }
+      let!(:slider3) { create(:slider, user: user, title: 'Overnight Sensation') }
+
+      before do
+        user.update_to_free!
+      end
 
       it 'shows the lock button' do
         visit dashboard_sliders_path
@@ -57,6 +74,29 @@ describe 'Sliders', js: true do
       it 'redirects to subscription on new slider' do
         visit new_dashboard_slider_path
         expect(page).to have_content('Current plan: Free')
+      end
+    end
+  end
+
+  describe 'premium plan' do
+    before do
+      user.update_to_premium!('cus_123')
+    end
+
+    context 'one or more sliders exist' do
+      let!(:slider1) { create(:slider, user: user, title: 'Hot Rats') }
+      let!(:slider2) { create(:slider, user: user, title: 'Apostrophe') }
+      let!(:slider3) { create(:slider, user: user, title: 'Overnight Sensation') }
+
+      it 'shows the add new slider button' do
+        visit dashboard_sliders_path
+        expect(page).to_not have_selector('.button--lock')
+        expect(page).to have_link('Add New Slider', href: new_dashboard_slider_path)
+      end
+
+      it 'does not show restricted sliders' do
+        visit dashboard_sliders_path
+        expect(page).to_not have_content('Access to the following sliders requires the Premium plan')
       end
     end
   end
