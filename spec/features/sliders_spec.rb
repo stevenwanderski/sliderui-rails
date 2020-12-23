b4d75e32b50d82e32508568b004b88bf44a66ad6
Steven <steven.wanderski@gmail.com>
2020-12-22 20:34:38 -0600

    Adds free plan feature specs

--- spec/features/sliders_spec.rb
+++ spec/features/sliders_spec.rb
@@ -36,4 +36,28 @@ describe 'Sliders', js: true do
 
     expect(page).to have_content("Slider \"Hot Rats\" was deleted.")
   end
+
+  describe 'free plan' do
+    context 'one or more sliders exist' do
+      let!(:slider1) { create(:slider, user: user, title: 'Hot Rats') }
+      let!(:slider2) { create(:slider, user: user, title: 'Apostrophe', restricted: true) }
+      let!(:slider3) { create(:slider, user: user, title: 'Overnight Sensation', restricted: true) }
+
+      it 'shows the lock button' do
+        visit dashboard_sliders_path
+        expect(page).to have_selector('.button--lock')
+        expect(page).to have_link('Add New Slider', href: edit_dashboard_subscription_path)
+      end
+
+      it 'shows the restricted sliders' do
+        visit dashboard_sliders_path
+        expect(page).to have_content('Access to the following sliders requires the Premium plan')
+      end
+
+      it 'redirects to subscription on new slider' do
+        visit new_dashboard_slider_path
+        expect(page).to have_content('Current plan: Free')
+      end
+    end
+  end
 end
