71babadc1f9cd4a88e96d7867a0c066217014b03
Steven <steven.wanderski@gmail.com>
2020-12-24 07:12:43 -0600

    Removes unused files

--- app/serializers/slider_serializer.rb
+++ app/serializers/slider_serializer.rb
@@ -1,11 +1,4 @@
 class SliderSerializer < ActiveModel::Serializer
   attributes :id, :title, :settings, :short_code, :display_image_url
-
   has_many :slides
-
-  def display_image_url
-    return nil if object.slides.empty?
-
-    object.slides.order(weight: :asc).first.image.url
-  end
 end
