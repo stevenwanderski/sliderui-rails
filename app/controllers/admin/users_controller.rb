85695576f5b935805e69b039706d672659f7cd39
Steven <steven.wanderski@gmail.com>
2021-01-07 07:53:30 -0600

    Adds admin user counts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < AdminController
   def index
     @users = User.order(created_at: :desc).page(params[:page]).per(50)
+    @free_count = User.where(subscription_type: 'free').count
+    @premium_count = User.where(subscription_type: 'premium').count
   end
 end
