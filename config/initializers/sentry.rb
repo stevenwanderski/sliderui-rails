0106a7509aa5d2806886bf165813399646648ad6
Steven Wanderski <steven.wanderski@gmail.com>
2023-03-28 07:47:14 -0500

    Adds production env to Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
 Sentry.init do |config|
   config.dsn = 'https://eb6a0dd9416c4ba1937e58bf9f787fbd:1e5dcc1a732340d1ab7ed5225d1301d4@sentry.io/120008'
-  config.environment = 'production'
+  config.enabled_environments = %w[ production ]
 end
