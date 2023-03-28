4584c8a21c4131838cb3444c17a34959fa9acd69
Steven Wanderski <steven.wanderski@gmail.com>
2023-03-28 07:19:01 -0500

    Upgrades Raven to Sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,4 @@
-Raven.configure do |config|
+Sentry.init do |config|
   config.dsn = 'https://eb6a0dd9416c4ba1937e58bf9f787fbd:1e5dcc1a732340d1ab7ed5225d1301d4@sentry.io/120008'
-  config.environments = %w[ production ]
-  config.processors -= [Raven::Processor::PostData]
-  config.excluded_exceptions = []
+  config.environment = 'production'
 end
