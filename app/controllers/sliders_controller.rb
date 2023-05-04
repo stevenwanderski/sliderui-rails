cdccf0622a8dad64007ccddeedc3d5d493b5b365
Steven Wanderski <steven.wanderski@gmail.com>
2023-05-04 13:17:16 -0500

    Enables request log creation

--- app/controllers/sliders_controller.rb
+++ app/controllers/sliders_controller.rb
@@ -5,28 +5,14 @@ class SlidersController < ApplicationController
   def show
     @slider = Slider.find_by(short_code: params[:short_code])
 
-    # begin
-    #   if params[:source] != 'dashboard'
-    #     RequestLog.create!(
-    #       slider: @slider,
-    #       user: @slider.user,
-    #       referrer: request.referrer
-    #     )
-    #   end
-    # rescue => e
-    #   # Oops.
-    # end
-
-    # @output = '<div class="slider">'
-    # @slider.slides.each do |slide|
-    #   if slide.image.present?
-    #     @output += '<div><img src="' + slide.image.url + '" /></div>'
-    #   end
-    # end
-    # @output += '</div>'
-
-    # respond_to do |format|
-    #   format.js
-    # end
+    begin
+      RequestLog.create!(
+        slider: @slider,
+        user: @slider.user,
+        referrer: request.referrer
+      )
+    rescue => e
+      # Oops.
+    end
   end
 end
