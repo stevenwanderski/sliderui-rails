deaf08be63c49cb2bd165a86654dadbdbb45c0ce
Steven Wanderski <steven.wanderski@gmail.com>
2024-02-07 13:34:54 -0600

    Adds 7.0 load defaults

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module Bxslider
   class Application < Rails::Application
+    config.load_defaults 7.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
