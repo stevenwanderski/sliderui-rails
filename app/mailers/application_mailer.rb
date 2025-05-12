a21cd076edfb7c91e7039f614b62e7c219d808da
Steven Wanderski <steven.wanderski@gmail.com>
2025-05-12 09:28:13 -0500

    Changes email to steven@happycode.llc

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   append_view_path Rails.root.join('app', 'views', 'mailers')
   default from: 'SliderUI <support@sliderui.com>'
-  default reply_to: 'SliderUI <support@sliderui.com>'
+  default reply_to: 'Steven <steven@happycode.llc>'
   layout 'mailer'
 end
